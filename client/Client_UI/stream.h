24b1fab272ed6cadc58987741b72998631b62d9b
LongPhanPip <82559091+LongPhanPip@users.noreply.github.com>
2021-09-03 20:38:57 +0700

    Update stream.h
    Add stream comment

--- client/Client_UI/stream.h
+++ client/Client_UI/stream.h
@@ -5,6 +5,12 @@
 
 #pragma comment(lib, "ws2_32.lib")
 
+/* Socket to connect to server
+    * @param	client_socket   socket
+    * @param	port_number	    portnumber
+    * @param	ip_address      ip of the server
+    
+*/
 class Socket {
 public:
     SOCKET client_socket;
@@ -15,10 +21,31 @@ public:
     Socket(int port_number, char* ip_address);
     ~Socket();
 
+    /* Set timeout for socket
+        * @param	time   [IN]    number of time
+        * @return NULL
+    */
     void set_timeout(int time);
+    
+    /* request to connect to server
+        * @return result_code
+    */
     int connect_to_server();
+    
+    /* Send data to server
+        * @param	code      [IN]    code of the packet
+        * @param	payload   [IN]    data to send to server
+        * @return result_code
+    */
     int tcp_send(char* code, char* payload);
+    
+    /* Receive data from server
+        * @param	code      [IN]    code of the packet
+        * @param	payload   [IN]    data to send to server
+        * @return result_code
+    */
     int tcp_receive(char*code, char* payload);
+    
     int Receive(char *buffer);
 };
 
