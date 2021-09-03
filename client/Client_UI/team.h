d8089313bec6b6fd5fc7c027b8bbae0625baec6e
LongPhanPip <82559091+LongPhanPip@users.noreply.github.com>
2021-09-03 20:42:22 +0700

    Update team.h
    Add team comment

--- client/Client_UI/team.h
+++ client/Client_UI/team.h
@@ -4,6 +4,21 @@
 #include "castle.h"
 #include "weapon.h"
 
+
+/* Store team data
+    * @param	id              id of the team
+    * @param	player_number	number of the player in team   
+    * @param	players         list player
+    * @param	castle_number   number of castle team has
+    * @param	castles         list castles team has
+    * @param	weapon          weapon of the team
+    * @param	wall            wall of the team
+    * @param	gold            amount of gold
+    * @param	wood            amount of wood
+    * @param	stone           amount of stone
+    * @param	iron            amount of iron
+    
+*/
 class Team {
 public:
     int id;
